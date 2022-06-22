f056eb99a37b1ba6f1a164b6bd876321728ee18a
rzakovich <rzakovich@apple.com>
2022-06-22 08:28:08 -0400

    Initial Commit

---
+++ config.js
@@ -0,0 +1,116 @@
+$(document).ready(() => {
+    tableau.extensions.initializeDialogAsync().then(function (openPayload) {
+        swapTriggerTypes();
+        getDashboardObjects();
+    });
+});
+
+function swapTriggerTypes() {
+    let triggerType = tableau.extensions.settings.get('triggerType');
+    $('.trigger-config').hide();
+    if (triggerType != undefined) {
+        $('#trigger-type option[value="' + triggerType + '"]').prop("selected", true);
+        $('#' + triggerType).show();
+        getParams();
+        setParamValue();
+        getWorksheets();
+    }
+    $('#trigger-type').change(function () {
+        $('.trigger-config').hide();
+        let triggerSelection = $(this).val();
+        if (triggerSelection == 'trigger-dashboard-action') {
+            getWorksheets();
+        } else {
+            getParams();
+        }
+        $('#' + $(this).val()).show();
+    })
+}
+
+function getWorksheets() {
+    let options = '';
+    let selectedWorksheet = tableau.extensions.settings.get('selectedWorksheet');
+    tableau.extensions.dashboardContent.dashboard.worksheets.forEach(function (worksheet) {
+        if (worksheet.name == selectedWorksheet) {
+            options += `<option value='${worksheet.name}' selected> ${worksheet.name}</option>`;
+        } else {
+            options += `<option value='${worksheet.name}'> ${worksheet.name}</option>`;
+        }
+    });
+    document.getElementById('selectedWorksheet').innerHTML = options;
+    document.getElementById('save-settings').disabled = false;
+}
+
+function getParams() {
+    let selectedParameter = tableau.extensions.settings.get('parameter');
+    tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(params => {
+        let options = '';
+        let c = 0;
+        for (let p of params) {
+            if (p.dataType != 'date') {
+                if (p.name == selectedParameter) {
+                    options += `<option value='${p.name}' selected>${p.name}</option>`;
+                } else {
+                    options += `<option value='${p.name}'>${p.name}</option>`;
+                }
+                c++
+            }
+        }
+        if (c > 0) {
+            document.getElementById('pickparam').innerHTML = options;
+            document.getElementById('save-settings').disabled = false;
+        } else {
+            document.getElementById('pickparam').innerHTML = "<option value='' disabled>No parameters found</option>";
+        }
+    });
+}
+
+function setParamValue() {
+    let configuedValue = tableau.extensions.settings.get('paramvalue');
+    if (configuedValue != undefined) {
+        document.getElementById('paramvalue').value = configuedValue;
+    }
+}
+
+function getDashboardObjects() {
+    let options = '';
+    let selectedZone = tableau.extensions.settings.get('zone');
+    tableau.extensions.dashboardContent.dashboard.objects.forEach(function (zone) {
+        if (zone.isFloating) {
+            if (zone.name == selectedZone) {
+                options += `<option value='${zone.name}' selected>${zone.name}</option>`;
+            } else {
+                options += `<option value='${zone.name}'>${zone.name}</option>`;
+            }
+        }
+    })
+    document.getElementById('pickzone').innerHTML = options;
+    document.getElementById('setzone').disabled = false;
+};
+
+function submit() {
+    let zone = document.getElementById('pickzone').value;
+    tableau.extensions.settings.set('zone', zone);
+
+    let triggerType = document.getElementById('trigger-type').value;
+    tableau.extensions.settings.set('triggerType', triggerType);
+
+    if (triggerType == 'trigger-parameter') {
+        let param = document.getElementById('pickparam').value;
+        let showhidevalue = document.getElementById('showhideoption').value;
+        let paramValue = document.getElementById('paramvalue').value;
+        tableau.extensions.settings.set('parameter', param);
+        tableau.extensions.settings.set('showhidevalue', showhidevalue);
+        tableau.extensions.settings.set('paramvalue', paramValue);
+    } else {
+        let selectedWorksheet = $('#selectedWorksheet').val();
+        tableau.extensions.settings.set('selectedWorksheet', selectedWorksheet);
+    }
+
+    tableau.extensions.settings.set('configured', 'true');
+
+    tableau.extensions.settings.saveAsync().then(result => {
+        tableau.extensions.ui.closeDialog("10");
+    });
+}
+
---
+++ main.js
@@ -0,0 +1,130 @@
+let selParam;
+let zoneVisbilityObject = {};
+
+$(document).ready(function () {
+    tableau.extensions.initializeAsync({ 'configure': configure }).then(() => {
+        console.log(tableau.extensions.settings.getAll());
+        let configured = (tableau.extensions.settings.get('configured') === 'true');
+        if (!configured) {
+            configure();
+        } else {
+            initExtension();
+        }
+    });
+});
+
+function configure() {
+    const popupUrl = `https://gbi-tableau.airflow.apple.com/objectvisibility/config`
+    let payload;
+    tableau.extensions.ui.displayDialogAsync(popupUrl, payload, { height: 400, width: 350 }).then((closePayload) => {
+        initExtension();
+    }).catch((error) => {
+        switch (error.errorCode) {
+            case tableau.ErrorCodes.DialogClosedByUser:
+                console.log("Dialog was closed by user.");
+                break;
+            default:
+                console.error(error.message);
+        }
+    });
+}
+
+async function initExtension() {
+    await setZoneVisibilityObject();
+    await setListenerType();
+    await setVisibility();
+}
+
+function setZoneVisibilityObject() {
+    tableau.extensions.dashboardContent.dashboard.objects.forEach(function (object) {
+        if (object.name == tableau.extensions.settings.get('zone')) {
+            zoneVisbilityObject[object.id] = tableau.ZoneVisibilityType.Show;
+        }
+    });
+}
+
+function setListenerType() {
+    let triggerType = tableau.extensions.settings.get('triggerType');
+    removeListener(triggerType);
+    if (triggerType == 'trigger-parameter') {
+        addParameterListener();
+    } else {
+        addWorksheetListener();
+    }
+}
+
+function removeListener(triggerType) {
+    if (triggerType == 'trigger-parameter') {
+        let selectedWorksheet = tableau.extensions.settings.get('selectedWorksheet');
+        if (selectedWorksheet != undefined) {
+            worksheet = tableau.extensions.dashboardContent.dashboard.worksheets.find(ws => ws.name === selectedWorksheet);
+            worksheet.removeEventListener(tableau.TableauEventType.MarkSelectionChanged, selection);
+        }
+    } else {
+        let selectedParameter = tableau.extensions.settings.get('parameter');
+        if (selectedParameter != undefined) {
+            tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(params => {
+                let selParam = params.find(p => p.name == selectedParameter);
+                selParam.removeEventListener(tableau.TableauEventType.ParameterChanged, setVisibility);
+            });
+        }
+    }
+}
+
+function addWorksheetListener() {
+    let selectedWorksheet = tableau.extensions.settings.get('selectedWorksheet');
+    worksheet = tableau.extensions.dashboardContent.dashboard.worksheets.find(ws => ws.name === selectedWorksheet);
+    worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, selection);
+}
+
+function addParameterListener() {
+    let selectedParameter = tableau.extensions.settings.get('parameter');
+    tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(params => {
+        let selParam = params.find(p => p.name == selectedParameter);
+        selParam.addEventListener(tableau.TableauEventType.ParameterChanged, setVisibility);
+    });
+}
+
+function getZoneVisibility(selParamValue) {
+    let configuedValue = tableau.extensions.settings.get('paramvalue');
+    let showZone = Boolean('show' == tableau.extensions.settings.get('showhidevalue'));
+    if (configuedValue == selParamValue && showZone) {
+        return tableau.ZoneVisibilityType.Show;
+    } else if (configuedValue != selParamValue && !showZone) {
+        return tableau.ZoneVisibilityType.Show;
+    } else {
+        return tableau.ZoneVisibilityType.Hide;
+    }
+}
+
+function setVisibility() {
+    let triggerType = tableau.extensions.settings.get('triggerType');
+    if (triggerType == 'trigger-parameter') {
+        tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(params => {
+            let selParamValue = params.find(p => p.name == tableau.extensions.settings.get('parameter')).currentValue.value;
+            let visibility = getZoneVisibility(selParamValue);
+            toggleZoneVisibility(visibility);
+        });
+    } else {
+        toggleZoneVisibility(tableau.ZoneVisibilityType.Hide);
+    }
+}
+
+function selection(data) {
+    data.getMarksAsync().then(marks => {
+        if (marks.data[0].data.length === 1) {
+            toggleZoneVisibility(tableau.ZoneVisibilityType.Show);
+        } else {
+            toggleZoneVisibility(tableau.ZoneVisibilityType.Hide);
+        }
+    })
+}
+
+function toggleZoneVisibility(visibility) {
+    for (let key in zoneVisbilityObject) {
+        zoneVisbilityObject[key] = visibility;
+    };
+    tableau.extensions.dashboardContent.dashboard.setZoneVisibilityAsync(zoneVisbilityObject).then(() => {
+        console.log('Visibility Changed');
+    });
+}
